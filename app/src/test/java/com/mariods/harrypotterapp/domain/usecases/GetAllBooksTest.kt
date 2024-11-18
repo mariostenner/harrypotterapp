1dcd5a48d9b9cbb148154b3f7f71160e5418b957
Mario DS <mariostenner@gmail.com>
2024-11-17 23:34:25 -0600

    Se agregaron los testing unitarios en los casos de uso

--- app/src/test/java/com/mariods/harrypotterapp/domain/usecases/GetAllBooksTest.kt
+++ app/src/test/java/com/mariods/harrypotterapp/domain/usecases/GetAllBooksTest.kt
@@ -1,20 +1,65 @@
 package com.mariods.harrypotterapp.domain.usecases
 
 import com.mariods.harrypotterapp.data.Repository
+import com.mariods.harrypotterapp.domain.model.BooksAttributes
+import com.mariods.harrypotterapp.domain.model.BooksList
+import dagger.hilt.android.testing.HiltAndroidRule
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.MockK
 import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
 import org.junit.Before
-import org.junit.jupiter.api.Assertions.*
+import org.junit.Rule
+import org.junit.Test
+import javax.inject.Inject
 
-class GetAllBooksTest{
+class GetAllBooksTest {
 
+    //@Inject
     @RelaxedMockK
     private lateinit var repository: Repository
-
     lateinit var getAllBooks: GetAllBooks
 
+
     @Before
-    fun onBefore(){
-       // MockAn
+    fun setup() {
+        //hiltRule.inject()
+        MockKAnnotations.init(this)
+        getAllBooks = GetAllBooks(repository)
+
+    }
+
+    //@get:Rule
+    //var hiltRule = HiltAndroidRule(this)
+
+    @Test
+    fun whenTheApiReturnSomethingThenGetValuesFromApi() = runBlocking {
+
+        //Given
+
+        val listBooksDummy: List<BooksList> =
+            listOf(
+                BooksList(
+                    id = "1",
+                    BooksAttributes(
+                        "Harry Potter and the big bug",
+                        "JMD Stenner",
+                        "https://www.image.com",
+                        777,
+                        "Today",
+                        "This is the story of a big bug in my code"
+                    )
+                )
+            )
+        coEvery { repository.getAllBooksFromApi() } returns listBooksDummy
+
+        //When
+        val response = getAllBooks()
+
+        //Then
+        assert(listBooksDummy == response)
+
     }
 
 }
---
+++ app/src/test/java/com/mariods/harrypotterapp/domain/usecases/GetAllCharactersTest.kt
@@ -0,0 +1,67 @@
+package com.mariods.harrypotterapp.domain.usecases
+
+import com.mariods.harrypotterapp.data.Repository
+import com.mariods.harrypotterapp.domain.model.BooksAttributes
+import com.mariods.harrypotterapp.domain.model.BooksList
+import com.mariods.harrypotterapp.domain.model.CharactersAttributes
+import com.mariods.harrypotterapp.domain.model.CharactersList
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.impl.annotations.RelaxedMockK
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.jupiter.api.Assertions.*
+import javax.inject.Inject
+
+@HiltAndroidTest
+class GetAllCharactersTest{
+
+    //@get:Rule
+    //val hiltRule = HiltAndroidRule(this)
+
+    //@Inject
+    @RelaxedMockK
+    lateinit var repository: Repository
+    lateinit var getAllCharacters: GetAllCharacters
+
+    @Before
+    fun setUp(){
+        MockKAnnotations.init(this)
+        getAllCharacters = GetAllCharacters(repository)
+        //hiltRule.inject()
+    }
+
+    @Test
+    fun whenTheApiReturnSomethingThenGetValuesFromApi() = runBlocking {
+
+        val listCharactersDummy: List<CharactersList> =
+        listOf(
+            CharactersList(
+                id = "1",
+                CharactersAttributes(
+                    "Harry Potter and the big bug",
+                    "JMD Stenner",
+                    "https://www.image.com",
+                    "777",
+                    "Today",
+                    "This is the story of a big bug in my code",
+                    "",
+                    ""
+                )
+            )
+        )
+
+        coEvery { repository.getAllCharactersFromApi() } returns listCharactersDummy
+
+        val response = getAllCharacters()
+
+        assert(listCharactersDummy == response)
+
+
+    }
+
+}
